9e7fb1f7dc4eb1db630e0d0fffc276f6f458dbb0
simonoppowa <simon.oppowa@st.oth-regensburg.de>
2018-06-15 19:48:30 +0200

    removed Google Ad library from paid productFlavor

--- app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
@@ -1,13 +1,11 @@
 package com.udacity.gradle.builditbigger.paid;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.udacity.gradle.builditbigger.R;
 
 import butterknife.ButterKnife;
